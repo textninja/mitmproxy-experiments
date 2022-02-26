e8c7ae24cb388a8505518f7c53fbb086fac977a4
Joe Taylor <joe@textninja.net>
2022-02-26 13:42:44 -0800

    added test for killed axios requests

--- test/frist-test.spec.js
+++ test/frist-test.spec.js
@@ -1,34 +1,53 @@
 import childprocess from 'child_process';
+import axios from 'axios';
 
-describe("frist test", () => {
+describe("response killer", () => {
     let subprocess;
 
     beforeAll(async () => {
-        subprocess = childprocess.spawn(
-            'mitmdump',
-            ['-s', 'scripts/kill-everything.py']
-        );
-
-        await new Promise((resolve) => {
-            let stdoutBuffer = '';
-
-            function waitForListeningMessage(chunk) {
-                stdoutBuffer += chunk.toString("utf8");
-                if (stdoutBuffer.match(/listening at http:\/\/\*:8080/)) {
-                    subprocess.stdout.off('data', waitForListeningMessage);
-                    resolve(true);
-                }
-            }
-
-            subprocess.stdout.on('data', waitForListeningMessage);
-        });
+        subprocess = await startProxyWithScript('scripts/kill-everything.py');
     });
 
-    it("should work", () => {
-        expect(1).toEqual(1);
+    it("should hang up when requesting google", async () => {
+        await expect(async () => {
+            await axios.get(
+                "https://www.google.com",
+                {
+                    "proxy": {
+                        "host": "localhost",
+                        "port": 8080
+                    }
+                }
+            );
+        })
+            .rejects
+            .toThrow("socket hang up");
     });
 
     afterAll(() => {
         subprocess.kill();
     });
 });
+
+async function startProxyWithScript(script) {
+    let subprocess = childprocess.spawn(
+        'mitmdump',
+        ['-s', script]
+    );
+
+    await new Promise((resolve) => {
+        let stdoutBuffer = '';
+
+        function waitForListeningMessage(chunk) {
+            stdoutBuffer += chunk.toString("utf8");
+            if (stdoutBuffer.match(/listening at http:\/\/\*:8080/)) {
+                subprocess.stdout.off('data', waitForListeningMessage);
+                resolve(true);
+            }
+        }
+
+        subprocess.stdout.on('data', waitForListeningMessage);
+    });
+
+    return subprocess;
+}
