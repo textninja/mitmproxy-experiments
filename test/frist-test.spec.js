0767ef08da1de3bec40f7dde5ebdf5d8a27b6fed
Joe Taylor <joe@textninja.net>
2022-02-26 13:00:54 -0800

    changed test to wait for mitmproxy to start

--- test/frist-test.spec.js
+++ test/frist-test.spec.js
@@ -3,23 +3,29 @@ import childprocess from 'child_process';
 describe("frist test", () => {
     let subprocess;
 
-    beforeAll(() => {
+    beforeAll(async () => {
         subprocess = childprocess.spawn(
             'mitmdump',
             ['-s', 'scripts/kill-everything.py']
         );
 
-        subprocess.stdout.on('data', (chunk) => {
-            process.stdout.write(chunk);
+        await new Promise((resolve) => {
+            let stdoutBuffer = '';
+
+            function waitForListeningMessage(chunk) {
+                stdoutBuffer += chunk.toString("utf8");
+                if (stdoutBuffer.match(/listening at http:\/\/\*:8080/)) {
+                    subprocess.stdout.off('data', waitForListeningMessage);
+                    resolve(true);
+                }
+            }
+
+            subprocess.stdout.on('data', waitForListeningMessage);
         });
     });
 
-    it("should work", async () => {
+    it("should work", () => {
         expect(1).toEqual(1);
-        await new Promise((resolve) => {
-            setTimeout(resolve, 2000);
-        });
-        expect(1+1).toEqual(2);
     });
 
     afterAll(() => {
