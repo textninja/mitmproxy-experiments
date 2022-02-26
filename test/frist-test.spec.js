3053366df51cd94ae0175adccca405ad61e67aeb
Joe Taylor <joe@textninja.net>
2022-02-26 12:51:17 -0800

    added code to open mitmproxy in tests

--- test/frist-test.spec.js
+++ test/frist-test.spec.js
@@ -1,9 +1,28 @@
+import childprocess from 'child_process';
+
 describe("frist test", () => {
+    let subprocess;
+
     beforeAll(() => {
-        console.log("starting");
+        subprocess = childprocess.spawn(
+            'mitmdump',
+            ['-s', 'kill-everything.py']
+        );
+
+        subprocess.stdout.on('data', (chunk) => {
+            process.stdout.write(chunk);
+        });
     });
 
-    it("should work", () => {
+    it("should work", async () => {
+        expect(1).toEqual(1);
+        await new Promise((resolve) => {
+            setTimeout(resolve, 2000);
+        });
         expect(1+1).toEqual(2);
+    });
+
+    afterAll(() => {
+        subprocess.kill();
     });
 });
